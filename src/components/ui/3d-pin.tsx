575d66a04a0e5f020d293c72f5a2baf317bbe5a0
stupidisnotavaliable <2977152213@qq.com>
2025-12-09 13:21:14 +0800

    3D卡片变形修复

--- src/components/ui/3d-pin.tsx
+++ src/components/ui/3d-pin.tsx
@@ -42,12 +42,16 @@ export const PinContainer = ({
         style={{
           perspective: "1000px",
           transform: "rotateX(70deg) translateZ(0deg)",
+          width: "100%",
+          maxWidth: "420px",
+          minWidth: "320px",
         }}
         className="absolute left-1/2 top-1/2 ml-[0.09375rem] mt-4 -translate-x-1/2 -translate-y-1/2"
       >
         <div
           style={{
             transform: transform,
+            width: "100%",
           }}
           className="absolute left-1/2 p-4 top-1/2 flex justify-start items-start rounded-2xl shadow-[0_8px_16px_rgb(0_0_0/0.4)] bg-black border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
         >
